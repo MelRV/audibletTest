cc5f41fe97e17418df71b86975df73a2a41eb3b8
Melissa Ramirez <melissaramirez@Melissas-MacBook-Air-2.local>
2024-03-11 12:05:25 -0600

    change test names

--- tests/audible_flow1.spec.ts
+++ tests/audible_flow1.spec.ts
@@ -1,16 +1,17 @@
 import { test, expect } from '@playwright/test';
-test.only('test', async ({ page }) => {
-  await page.goto('https://www.audible.com/ap/signin');
-  await page.getByRole('link', { name: 'Home' }).click();
-  await page.getByRole('link', { name: 'Entrar' }).click();
-  await page.getByLabel('E-mail ou número de telefone').click();
-  await page.getByLabel('E-mail ou número de telefone').fill('me');
-  await page.getByLabel('E-mail ou número de telefone').click();
-  await page.getByLabel('E-mail ou número de telefone').fill('melissaramirezvillegas@gmail.com');
-  await page.getByLabel('E-mail ou número de telefone').press('Enter');
-  await page.getByLabel('Senha').fill('OlinPruebas123');
-  await page.getByLabel('Faça seu login').click();
-  await page.getByRole('link', { name: 'Oi, Olinprueba!' }).click();
-  await page.getByRole('link', { name: 'Oi, Olinprueba!' }).click();
-  await page.getByRole('link', { name: 'Sair' }).click();
-  });
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.audible.com/');
+  await page.getByRole('link', { name: 'Sign in' }).click();
+  await page.getByLabel('Email or mobile phone number').click();
+  await page.getByLabel('Email or mobile phone number').fill('melissaramirezvillegas@gmail.com');
+  await page.getByLabel('Email or mobile phone number').press('Enter');
+  await page.getByLabel('Password').press('CapsLock');
+  await page.getByLabel('Password').fill('');
+  await page.getByLabel('Password').press('CapsLock');
+  await page.getByLabel('Password').fill('Olin');
+  await page.getByLabel('Password').press('CapsLock');
+  await page.getByLabel('Password').fill('OlinPruebas123');
+  await page.getByLabel('Password').press('Enter');
+  await page.getByRole('link', { name: 'Hi, Olinprueba!' }).click();
+});
--- tests/audible_flow2.spec.ts
+++ tests/audible_flow2.spec.ts
@@ -5,7 +5,9 @@ test('Search specific products by keyword', async({page}) =>{
     
 
     await page.goto('https://www.audible.com/');
+    await page.pause()
   await page.getByRole('link', { name: 'Iniciar sesión' }).click();
+  await page.pause()
   await page.getByLabel('E-mail o celular').click();
   await page.getByLabel('E-mail o celular').fill('melissaramirezvillegas@gmail.com');
   await page.getByLabel('Continuar').click();
@@ -14,7 +16,9 @@ test('Search specific products by keyword', async({page}) =>{
   await page.getByLabel('Contraseña').press('CapsLock');
   await page.getByLabel('Contraseña').fill('OlinPruebas123');
   await page.getByLabel('Ingresar').click();
+  await page.pause()
   await page.getByPlaceholder('Find your next great listen').click();
+  await page.pause()
   await page.getByPlaceholder('Find your next great listen').press('CapsLock');
   await page.getByPlaceholder('Find your next great listen').fill('Proyecto ');
   await page.getByPlaceholder('Find your next great listen').press('CapsLock');
